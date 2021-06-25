bbaf5a7de359cd5a833fea8c2f293fabc3113f47
danielbyun <dbyun92@gmail.com>
2021-06-25 18:22:51 +0900

    commit test

--- src/styles/media-queries.js
+++ src/styles/media-queries.js
@@ -1,5 +1,5 @@
-const large = '@media (min-width: 1200px)'
-const medium = '@media (min-width: 992px) and (max-width: 1199px)'
 const small = '@media (max-width: 991px)'
+const medium = '@media (min-width: 992px) and (max-width: 1199px)'
+const large = '@media (min-width: 1200px)'
 
 export {small, medium, large}
